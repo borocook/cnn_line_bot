c791b3bf66df9a4d989f9ef6c9135b1c3ee7b9b4
borocook <buncook5353@gmail.com>
2020-10-11 23:35:09 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    FollowEvent, MessageEvent, TextMessage, ImageMessage, TextSendMessage,
+)
+import os
+
+import unown_class
+
+app = Flask(__name__)
+
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+#hello_mes = '{Nickname}さん、はじめまして！友だち追加ありがとうございます。{AccountName}です。'
+#+'ポケモンのアンノーンの画像を送信して頂くと自動でAIが判別して結果を返信します。'
+#+'【画像の注意事項】１．画像はなるべく隙間を無くしてください。２．背景の色は統一してください。'
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='初めまして')
+    )
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+@handler.add(MessageEvent, message=(ImageMessage))
+def handle_image_message(event):
+    content = line_bot_api.get_message_content(event.message.id)
+    img_dirname = event.message.id + ".jpg"
+    with open(img_dirname, "wb") as f:
+        for chunk in content.iter_content():
+            f.write(chunk)
+    result = unown_class.Classification(img_dirname)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=result))
+    
+    
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
---
+++ unown_class.py
@@ -0,0 +1,54 @@
+import math
+from PIL import Image  #Pillow
+from tensorflow.keras.preprocessing.image import img_to_array #tensorflow
+from tensorflow.keras.models import model_from_json
+
+def Classification(img_name):
+  str = [chr(i) for i in range(97, 97+26)]
+  str.insert(0, '!')
+  str.insert(1, '?')
+
+  img = Image.open(img_name)
+  width, height = img.size
+
+  img = img.convert('RGB')
+  if width != 50 or height != 50 :
+    if width == height:
+      tmp = img.resize((50, 50))
+    else:
+      if width < height:
+        side = height
+        x = math.floor((height - width) / 2)
+        y = 0
+      else:
+        side = width
+        x = 0
+        y = math.floor((width - height) / 2)
+      tmp = Image.new('RGB', (side, side), 'white')
+      tmp.paste(img, (x, y))
+      tmp = tmp.resize((50, 50))
+      print('resize complete')
+  else:
+      tmp = img
+
+  array_img = img_to_array(tmp).reshape(1, 50, 50, 3).astype('float32') / 255.0
+
+  model = model_from_json(open('./unown_model.json').read())
+  model.load_weights('./unown_model_weights.hdf5')
+
+  pred = model.predict(array_img, batch_size=1, verbose=1)
+  max_str = pred.argmax() #ndarrayの最大値のインデックス取得
+  prob = pred.tolist()
+  prob = [round(prob[0][i]* 100, 1) for i in range(len(prob[0]))]
+
+  n = 0
+  alp_max = ""
+  rate_max = 0  
+
+  for chra in enumerate(str):
+    if rate_max < prob[n]:
+      alp_max = chra[1]
+      rate_max = prob[n]
+    n += 1
+  
+  return alp_max
