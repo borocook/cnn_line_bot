99cae2f449cb7ee70e45ca0d476fc9ac7992bb95
borocook <buncook5353@gmail.com>
2020-11-19 18:43:54 +0900

    commit

--- main.py
+++ main.py
@@ -40,10 +40,11 @@ def callback():
     return 'OK'
 
 #hello_body = "{Nickname}さん、はじめまして！友だち追加ありがとうございます。{AccountName}です。"
-message_body = "ポケモンのアンノーンの画像を送信して頂くと自動でAIが判別して結果を返信します。\n【画像の注意事項】\n１．画像はなるべく隙間を無くしてください。２．背景の色は統一してください。"
+message_body = "ポケモンのアンノーンの画像を送信して頂くと自動でAIが判別して結果を返信します。\n【画像の注意事項】\n１．画像はなるべく隙間を無くしてください。\n ２．背景の色は統一してください。"
 
 @handler.add(FollowEvent)
 def handle_follow(event):
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=hello_body)
@@ -51,9 +52,20 @@ def handle_follow(event):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
+    if status_msg != "None":
+        # LINEに登録されているstatus_messageが空の場合は、"なし"という文字列を代わりの値とする
+        status_msg = "なし"
+    messages = TemplateSendMessage(alt_text="Buttons template",
+                                   template=ButtonsTemplate(
+                                       thumbnail_image_url=profile.picture_url,
+                                       title=profile.display_name,
+                                       text=f"User Id: {profile.user_id[:5]}...\n"
+                                            f"Status Message: {status_msg}",
+                                       actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=message_body))
+        TextSendMessage(text=messages))
 
 @handler.add(MessageEvent, message=(ImageMessage))
 def handle_image_message(event):
